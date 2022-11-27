b54d4371e49bb856338963460ea364102726f896
E9uieR <dshs1130@gmail.com>
2022-11-27 15:52:47 +0900

    reviewsテーブル作成

---
+++ database/migrations/2022_11_27_154756_create_reviews_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->string('review_title');
+            $table->string('review_body');
+            $table->integer('star');
+            $table->foreignId('user_id_review')->constrained(); //外部キー
+            $table->foreignId('user_id_reviewed')->constrained(); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
