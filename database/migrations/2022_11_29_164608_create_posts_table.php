608902c199502a6240c5b372c678dbe893d56144
E9uieR <dshs1130@gmail.com>
2022-12-06 10:02:35 +0900

    migration一式

---
+++ database/migrations/2022_11_29_164608_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('post_title');
+            $table->string('post_body');
+            $table->foreignId('user_id_post')->constrained(); //外部キー
+            $table->foreignId('categorie_id')->constrained();
+            $table->foreignId('game_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
---
+++ database/migrations/2022_12_05_232020_create_categoire_game_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categoire_game', function (Blueprint $table) {
+            $table->unsignedBigInteger('categoire_id');
+            $table->unsignedBigInteger('game_id');
+            // 複合主キーを定義
+            $table->primary(['categoire_id','game_id']);
+            // 指定したカラムに外部キー制約を定義
+            $table->foreign('categoire_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->foreign('game_id')->references('id')->on('games')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categoire_game');
+    }
+};
